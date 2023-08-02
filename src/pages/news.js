3f762a351336e3e3be995ddc71be21b1b618d10b
chanshiii <m.gaki1020skytree0503@gmail.com>
2023-08-02 16:04:18 +0900

    commit massege

---
+++ src/pages/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../styles/test.module.scss';
+
+const news = () => {
+  return (
+    <divc className='text-5xl underline'>
+        news
+        <p className={style.test}>テスト</p>
+    </divc>
+  )
+}
+
+export default news
