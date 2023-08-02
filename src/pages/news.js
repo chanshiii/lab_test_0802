335aa218aa2a62636f45df980eaf4ff6786240de
chanshiii <m.gaki1020skytree0503@gmail.com>
2023-08-02 17:43:49 +0900

    slick追加

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import style from '../styles/test.module.scss';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const news = () => {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1
+    };
   return (
-    <divc className='text-5xl underline'>
-        news
-        <p className={style.test}>テスト</p>
-    </divc>
+        <Slider {...settings}>
+        <div className={style.a}>
+          <img src="https://gsacademy.jp/uploads/hirotasan-eyecatch-720x720.jpeg" alt="Slide 1" />
+        </div>
+        <div className={style.a}>
+          <img src="https://gsacademy.jp/uploads/3-4.png" alt="Slide 2" />
+        </div>
+        <div className={style.a}>
+          <img src="https://gsacademy.jp/uploads/IMG_7963-720x720.jpg" alt="Slide 3" />
+        </div>
+      </Slider>
   )
 }
-
 export default news
